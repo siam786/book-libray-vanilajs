6803b830fe4edadeeedb1bcb01ae7545299cc7a3
siam786 <shohrab.hossain36@gmail.com>
2021-09-02 22:07:48 +0600

    final version

--- app.js
+++ app.js
@@ -9,44 +9,47 @@ document.getElementById("search-field").onkeypress = function (e) {
   }
 };
 
-//error
-const errorDiv = document.getElementById('error');
+//error hnadle
+const errorDiv = document.getElementById("error");
 
 //add book function
 const searchBook = () => {
   const searchField = document.getElementById("search-field");
   spinner("block");
   const searchText = searchField.value;
-  /*   if(searchText === ''){
-      alert('no')
-    } */
- 
-  searchField.value =''
-  //load spinner
+// error handke search when empty
+  if (searchText === "") {
+    errorDiv.innerText = "Search Field Cannot be empty";
+    spinner("none");
+  }
+  else{
+  // reset search field
+  searchField.value = "";
   //load api data
   const url = `https://openlibrary.org/search.json?q=${searchText}`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => bookResult(data.docs));
+    errorDiv.innerText=''
+}
 };
 
+// book result function
 const bookResult = (books) => {
-    //search result 
-const searchShow = document.getElementById('serch-result').innerHTML = `
+  //search result
+  const searchShow = (document.getElementById("serch-result").innerHTML = `
 <h3 class="text-center py-3">Total Result Found <span class="text-danger">${books.length}</span></h3>
-`
-  //console.log(singleBook)
+`);
   const booksWrapper = document.getElementById("searchresult");
   //clea search book result
-  booksWrapper.textContent = ''
+  booksWrapper.textContent = "";
   books?.forEach((book) => {
-    //console.log(book);
     const div = document.createElement("div");
     div.classList.add("col");
     div.innerHTML = `
     <div class="card">
         <div class="card-body">
-        <img class="img-fluid card-img-top" src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg"/>
+            <img class="img-fluid card-img-top" src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg"/>
             <h4 class="card-title">Book Name: ${book.title}</h4>
             <h6 class="card-title">Author Name: ${book.author_name}</h6>
             <p class="card-text">First Publish year: 
@@ -57,7 +60,25 @@ const searchShow = document.getElementById('serch-result').innerHTML = `
             </p>
         </div>
     </div>`;
-    booksWrapper.appendChild(div)
-    spinner('none')
+    // wehn book cover image not found
+    if (book.cover_i === book[-1]) {
+      div.innerHTML = `
+        <div class="card">
+            <div class="card-body">
+            <img class="img-fluid card-img-top" src="images/no-img.png"/>
+                <h4 class="card-title">Book Name: ${book.title}</h4>
+                <h6 class="card-title">Author Name: ${book.author_name}</h6>
+                <p class="card-text">First Publish year: 
+                    ${book.first_publish_year}
+                </p> 
+                <p class="card-text">Publisher: 
+                    ${book.publisher}
+                </p>
+            </div>
+        </div>`;
+    }
+    booksWrapper.appendChild(div);
+    //spiner die
+    spinner("none");
   });
 };
